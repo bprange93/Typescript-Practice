4fbfbeab4ac57d994e299071c288f9c69a713dbc
Brandon Prange <bprange93@gmail.com>
2021-03-11 12:28:04 -0600

    Learned about the different ways for Type Assertion. Next will be going over Arrow Functions.

--- main.ts
+++ main.ts
@@ -1,13 +1,4 @@
-let a: number;
-let b: boolean;
-let c: string;
-let d: any;
-let e: number[] = [1, 2, 3];
-let f: any[] = [1, true, 'a', false];
-
-const colorRed = 0;
-const colorGreen = 1;
-const colorBlue = 2;
-
-enum Color { red = 0, green = 1, blue = 2 };
-let backgroundColor = Color.red;
+let message;
+message = 'abc';
+let endsWithC = (<string>message).endsWith('c');
+let alternativeWay = (message as string).endsWith('c');
