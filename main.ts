fcad5e8303bb3874045b02a7608c2c2888cfde15
Brandon Prange <bprange93@gmail.com>
2021-03-11 13:36:31 -0600

    Classes were learned about. Were able to take the functions and fields and put them inside of the class for cleaner code. Inside of the class the functions are referred to as fields. Next will be about Objects.

--- main.ts
+++ main.ts
@@ -1,24 +1,35 @@
-// You don't want to do it like this because there are too many perameters. 
-// let drawPoint = (x, y, a, b, c, d, e) => {
+
+//Changed to class to apply Cohesion Principle here. X and Y fields, draw is a Function
+class Point {
+    x: number;
+    y: number;
+    //In class function is referred to as a method rather than a function. 
+    draw(){
+        //...
+    }
+
+    getDistance(another: Point){
+        //...
+    }
+}
+
+//These were taken out for better coding practices, makes the code cleaner and easier to read. They were no longer needed since methods
+//were put in the class Point. 
+// let drawPoint = (point: Point) => {
 //     // ...
 // }
 
-//this is how to incoporate interfaces in Typescript, always use uppercase when using interfaces. 
-interface Point {
-    x: number,
-    y: number
-}
-
-let drawPoint = (point: Point) => {
-    // ...
-}
+// let getDistance = (pointA: Point, pointB: Point) => {
+//     //...
+// }
 
 //Inline annotation works for simple cases.
 // let drawPoint = (point: { x: number, y: number}) => {
 //      // ...
 // }
 
-drawPoint({
-    x: 1,
-    y: 2
-})
+//No longer needed as well since draw() was put into the class and uses the fields in that class. 
+// drawPoint({
+//     x: 1,
+//     y: 2
+// })
