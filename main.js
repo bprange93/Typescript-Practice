2d3693ddef5afc6abadb10873431acac5d67923d
Brandon Prange <bprange93@gmail.com>
2021-03-11 11:45:07 -0600

    Worked with enum, showed how it is much cleaner to use in TypeScript than it is in JavaScript. Moving on to the next lesson.

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('finally: ' + i);
-}
-doSomething();
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var colorRed = 0;
+var colorGreen = 1;
+var colorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["red"] = 0] = "red";
+    Color[Color["green"] = 1] = "green";
+    Color[Color["blue"] = 2] = "blue";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.red;
--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
-function doSomething(){
-    for (var i = 0; i < 5; i++) {
-        console.log(i)
-    }
-    console.log('finally: ' + i);
-}
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, 'a', false];
 
-doSomething();
+const colorRed = 0;
+const colorGreen = 1;
+const colorBlue = 2;
+
+enum Color { red = 0, green = 1, blue = 2 };
+let backgroundColor = Color.red;
