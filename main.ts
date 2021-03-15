5dff7c9657f6b8440adfaefed12cdf30e384e100
Brandon Prange <bprange93@gmail.com>
2021-03-15 15:40:15 -0500

    Went over how to do get, set in TypeScript. Cleaned up the code and took out some of the commented out code since it will be in commits from before. F2 is a good command to use when trying to change all variables that match in the code to something else you want to name it.

--- main.ts
+++ main.ts
@@ -2,36 +2,28 @@
 //Changed to class to apply Cohesion Principle here. X and Y fields, draw is a Function
 class Point {
 
-    constructor(private x?: number, private y?: number){
+    constructor(private _x?: number, private _y?: number){
 
     }    
     //In class function is referred to as a method rather than a function. 
     draw(){
-        console.log('x: ' + this.x + ', Y: ' + this.y);
+        console.log('x: ' + this._x + ', Y: ' + this._y);
+    }
+    
+    get x(){
+        return this._x;
+    }
+
+    set x(value){
+        if (value < 0)
+        throw new Error('value cannot be less than 0');
+
+        this._x = value;
     }
 
 }
 //object instance of a class.
 let point = new Point(1, 2);
+let x = point.x;
+point.x = 10;
 point.draw();
-
-//These were taken out for better coding practices, makes the code cleaner and easier to read. They were no longer needed since methods
-//were put in the class Point. 
-// let drawPoint = (point: Point) => {
-//     // ...
-// }
-
-// let getDistance = (pointA: Point, pointB: Point) => {
-//     //...
-// }
-
-//Inline annotation works for simple cases.
-// let drawPoint = (point: { x: number, y: number}) => {
-//      // ...
-// }
-
-//No longer needed as well since draw() was put into the class and uses the fields in that class. 
-// drawPoint({
-//     x: 1,
-//     y: 2
-// })
